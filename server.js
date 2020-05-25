514499b53338039d56aafb44e84eb3f11f6e584e
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-25 23:09:02 +0100

    Deixando a API disponivel com Cros

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 //Inicializando o app
 const port = 3000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 //iniciando DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
